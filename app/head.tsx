59e6a372b0faaf4a921fe3d3bb8b68618d17fbea
ryanfurrh <ryanfurrh@gmail.com>
2023-03-27 19:32:14 -0400

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>flux</title>
+      <title>primaevus</title>
       <meta content="width=device-width, initial-scale=1" name="" />
       <link rel="icon" href="/favicon.ico" />
     </>
