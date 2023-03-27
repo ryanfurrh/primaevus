2a4a60d60fdaf062b4980c8e6be68f10849c7dba
ryanfurrh <ryanfurrh@gmail.com>
2023-03-27 19:52:09 -0400

    Add readable meta data
    with gray matter

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,36 @@
 import fs from "fs";
 import Link from "next/link";
 import matter from "gray-matter";
+import { PostMetadata } from "@/components/PostMetadata";
 
-const getPostMetadata = () => {
+const getPostMetadata = (): PostMetadata[] => {
   const folder = "posts/";
   const files = fs.readdirSync(folder);
   const markdownPosts = files.filter((file) => file.endsWith(".md"));
-  const slugs = markdownPosts.map((file) => file.replace(".md", ""));
-  return slugs;
+
+  const posts = markdownPosts.map((fileName) => {
+    const fileContents = fs.readFileSync(`posts/${fileName}`, "utf8");
+    const matterResult = matter(fileContents);
+    return {
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+      subtitle: matterResult.data.subtitle,
+      slug: fileName.replace(".md", ""),
+    };
+  });
+
+  return posts;
 };
 
 const HomePage = () => {
   const postMetadata = getPostMetadata();
-  const postPreviews = postMetadata.map((slug) => (
+  const postPreviews = postMetadata.map((post) => (
     <div>
-      <Link href={`/posts/${slug}`}>
-        <h2>{slug}</h2>
+      <Link href={`/posts/${post.slug}`}>
+        <h2>{post.title}</h2>
       </Link>
+      <p>{post.subtitle}</p>
+      <p>{post.date}</p>
     </div>
   ));
 
