a91986a0b8d3d9ccb13b49c7315e2365b088579b
ryanfurrh <ryanfurrh@gmail.com>
2023-03-28 15:09:40 -0400

    Finishing blog stuff
    not entirely sure what is going on with this, but it works!

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,11 @@
-import fs from "fs";
+import PostPreview from "@/components/PostPreview";
+import getPostMetadata from "@/components/getPostMetadata";
 import Link from "next/link";
-import matter from "gray-matter";
-import { PostMetadata } from "@/components/PostMetadata";
-
-const getPostMetadata = (): PostMetadata[] => {
-  const folder = "posts/";
-  const files = fs.readdirSync(folder);
-  const markdownPosts = files.filter((file) => file.endsWith(".md"));
-
-  const posts = markdownPosts.map((fileName) => {
-    const fileContents = fs.readFileSync(`posts/${fileName}`, "utf8");
-    const matterResult = matter(fileContents);
-    return {
-      title: matterResult.data.title,
-      date: matterResult.data.date,
-      subtitle: matterResult.data.subtitle,
-      slug: fileName.replace(".md", ""),
-    };
-  });
-
-  return posts;
-};
 
 const HomePage = () => {
   const postMetadata = getPostMetadata();
   const postPreviews = postMetadata.map((post) => (
-    <div>
-      <Link href={`/posts/${post.slug}`}>
-        <h2>{post.title}</h2>
-      </Link>
-      <p>{post.subtitle}</p>
-      <p>{post.date}</p>
-    </div>
+    <PostPreview key={post.slug} {...post} />
   ));
 
   return <div>{postPreviews}</div>;
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,20 +1,30 @@
 import fs from "fs";
 import Markdown from "markdown-to-jsx";
+import matter from "gray-matter";
+import getPostMetadata from "@/components/getPostMetadata";
 
 const getPostContent = (slug: string) => {
   const folder = "posts/";
   const file = `${folder}${slug}.md`;
   const content = fs.readFileSync(file, "utf8");
-  return content;
+  const matterResult = matter(content);
+  return matterResult;
+};
+
+export const generateStaticParams = async () => {
+  const posts = getPostMetadata();
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
 };
 
 const PostPage = (props: any) => {
   const slug = props.params.slug;
-  const content = getPostContent(slug);
+  const post = getPostContent(slug);
   return (
     <div>
-      <h1>this is a post: {slug}</h1>
-      <Markdown>{content}</Markdown>
+      <h1>{post.data.title}</h1>
+      <Markdown>{post.content}</Markdown>
     </div>
   );
 };
