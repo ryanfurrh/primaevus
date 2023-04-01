0552f2a0d75c7e476903eadd507b2fa34422debb
ryanfurrh <ryanfurrh@gmail.com>
2023-03-31 21:41:56 -0400

    Create and import footer component to rootlayout

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+export function Footer() {
+  return (
+    <footer>
+      <div
+        className="border-stone-300 border-t-2 py-6 mt-6 text-center text-stone-400
+         italic"
+      >
+        <h3>from the primordium</h3>
+      </div>
+    </footer>
+  );
+}
