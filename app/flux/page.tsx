a026587fc89740947016d52627a6cc3039266570
ryanfurrh <ryanfurrh@gmail.com>
2023-04-20 16:07:59 -0700

    Add flux responsiveness

--- app/flux/page.tsx
+++ app/flux/page.tsx
@@ -12,29 +12,38 @@ export default function Page() {
       <div className="">
         <PageHeader name={metadata.title} description={metadata.description} />
       </div>
-      <div className="ml-52 px-12">
-        <h1>coming soon</h1>
-        <div className="w-1/4">
-          <div className="flex flex-row w-full justify-between p-4">
+      <div className="md:px-12 flex flex-col items-center">
+        <div className="w-2/3 items-center">
+          <div className="flex h-auto justify-center gap-9 md:gap-16">
             <Image
               src="/gifs/construction-1.gif"
               alt={""}
               width={48}
               height={48}
+              className="shrink-0"
             />
             <Image
               src="/gifs/construction-2.gif"
               alt={""}
               width={100}
               height={50}
+              className="shrink-0"
             />
             <Image
               src="/gifs/construction-1.gif"
               alt={""}
               width={48}
               height={48}
+              className="shrink-0"
             />
           </div>
+          <Image
+            src="/images/flux-illustration-1.svg"
+            alt={""}
+            width={175}
+            height={250}
+            className="mx-auto mt-12 border-2 border-stone-300 border-t-[10px] p-2 "
+          />
         </div>
       </div>
     </div>
