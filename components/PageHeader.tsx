2e808e71e36db8cb219238863b981bd37e8e1f31
ryanfurrh <ryanfurrh@gmail.com>
2023-04-20 16:03:12 -0700

    Update page header

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -4,11 +4,11 @@ import { NavIndex, type Item } from "@/app/Nav-Index";
 
 export function PageHeader(props: { name: string; description: string }) {
   return (
-    <header>
-      <div className="text-left bg-stone-400 p-4 mb-6 w-screen pl-64">
-        <h1 className="text-xl font-bold text-stone-500">{props.name}</h1>
-        <p className="text-stone-500/70">{props.description}</p>
-      </div>
-    </header>
+    <div className="flex flex-auto px-4 md:px-12 pt-4 pb-0 mb-6 text-center justify-center">
+      <h1 className="text-2xl font-bold text-stone-50 bg-stone-500 px-12 md:px-16">
+        {props.name}
+      </h1>
+      <p className="text-stone-500/70">{props.description}</p>
+    </div>
   );
 }
