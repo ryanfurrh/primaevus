3e3c77187dd262dc92fe56f8c0102e35d302590f
ryanfurrh <ryanfurrh@gmail.com>
2023-05-10 18:35:02 -0400

    Add close icon

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -6,3 +6,4 @@ export { default as DeveloperLogIcon} from './devlog.svg'
 
 export { default as PrimaeLogo } from './primae-logo-1.svg'
 export { default as NoImage } from './noimage.svg'
+export { default as CloseIcon } from './close.svg'
