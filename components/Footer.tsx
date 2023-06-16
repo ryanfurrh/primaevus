14f05de5b73d6488dc914c77f0446930aee2c3d3
ryanfurrh <ryanfurrh@gmail.com>
2023-06-16 17:46:44 -0400

    Update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,8 @@
 export function Footer() {
   return (
     <footer>
-      <div
-        className="w-full border-sand-200 border-t-2 py-3 mt-6 z-5 text-center bg-sand-50 text-sand-300
-         italic"
-      >
-        <h3 className="">[from the primordium]</h3>
+      <div className="w-full py-4 mt-6 text-xs italic text-center bg-transparent border-t-1 border-sand-300 dark:border-pale-100/40 z-5 text-sand-400 dark:text-pale-100/50">
+        <h3 className=" md:pl-[124px]">[from the primordium]</h3>
       </div>
     </footer>
   );
