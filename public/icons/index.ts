656ead7a024a9268bfbab9a43ae16918a57f6b73
ryanfurrh <ryanfurrh@gmail.com>
2023-06-16 17:48:01 -0400

    Add and update icons

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -1,9 +1,15 @@
+// // PAGE ICONS //
 export { default as FluxIcon} from './fluxicon.svg'
 export { default as ArtifactDatabaseIcon} from './artifactdatabase.svg'
 export { default as WorldIcon} from './theworld.svg'
-export { default as DeveloperLogIcon} from './devlog.svg'
+export { default as DocumentationIcon} from './documentation.svg'
 
-
-export { default as PrimaeLogo } from './primae-logo-1.svg'
-export { default as NoImage } from './noimage.svg'
+// // OPERATIONAL ICONS //
 export { default as CloseIcon } from './close.svg'
+export { default as PrimaeLogo } from './primae-logo-1.svg'
+export { default as PrimaeLogoMinimal } from './primae-logo-2.svg'
+export { default as NoImage } from './noimage.svg'
+export { default as ArrowSelect } from './arrow-select.svg'
+export { default as ArrowLeft } from './arrow-left.svg'
+export { default as Close } from './close.svg'
+export { default as Menu } from './menu.svg'
