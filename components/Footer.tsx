2b8f761b192042fbc3ea4df2781241044531fa70
ryanfurrh <ryanfurrh@gmail.com>
2023-09-04 02:28:07 -0400

    Add version to footer for mobile

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,16 @@
+import getConfig from "next/config";
+import Version from "./Version";
+
+const { version } = require("/package.json");
+
 export function Footer() {
   return (
     <footer>
-      <div className="w-full py-4 mt-6 text-xs italic text-center bg-transparent border-t-1 border-sand-300 dark:border-pale-100/40 z-5 text-sand-400 dark:text-pale-100/50">
-        <h3 className=" md:pl-[124px]">[from the primordium]</h3>
+      <div className="w-full py-4 mt-6 text-xs md:pl-[124px] gap-2 flex flex-col italic text-center bg-transparent border-t-1 border-sand-300 dark:border-pale-100/40 z-5 text-sand-400 dark:text-pale-100/50">
+        <h3 className="">[from the primordium]</h3>
+        <div className="flex justify-center md:hidden">
+          <Version />
+        </div>
       </div>
     </footer>
   );
