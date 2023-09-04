b9f3460c5c1e815df56dceb28b658ee6c93c55c8
ryanfurrh <ryanfurrh@gmail.com>
2023-09-04 02:32:16 -0400

    Create index to list all artifacts and their data

---
+++ app/artifacts/ArtifactDatabase/ArtifactIndex.tsx
@@ -0,0 +1,31 @@
+export const ArtifactIndex: {
+  name: string;
+  file: string;
+  scale: number;
+  date: string;
+  use: string;
+}[] = [
+  {
+    name: "Artifact A",
+    file: "models/personal_computer/scene2.gltf",
+    scale: 3.5,
+    date: "2023/05/15",
+    use: "Proximum proficere consentit Mario emergant mares eandem cruciatus Rome clarus. Exoriens dispersa interlunio decidant Scipionibus.  ",
+  },
+
+  {
+    name: "Artifact B",
+    file: "models/cell-amplifiers-3g/scene.gltf",
+    scale: 2,
+    date: "2023/05/15",
+    use: "Civilia caelestium inclusa.  Turbinis locris docuere Dionysio molibus dependentes crinem.  Lapides vel molis.  Exspuitur ipsos ventus scandere longissima.",
+  },
+
+  {
+    name: "Artifact C",
+    file: "models/satellitedish/satellitedish.gltf",
+    scale: 2,
+    date: "2023/05/15",
+    use: "Inclusa litus altitudinum subsolanus lumen stationes.  Invalida Porcio apsides admixtis.  Derasidas totus duas mutata.  Septentrio subiacet transgressis. ",
+  },
+];
