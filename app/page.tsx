8835bd61d39d4a841289838232a3851f7f4669cf
ryanfurrh <ryanfurrh@gmail.com>
2023-09-04 02:29:52 -0400

    Update homepage 3d model

--- app/page.tsx
+++ app/page.tsx
@@ -50,31 +50,59 @@ export default function HomePage() {
           />
         </div>
 
-        <div className="flex w-full mb-12 h-[300px] md:h-[500px] hover:cursor-grab active:cursor-grabbing">
-          <Canvas
-            shadows
-            dpr={[1, 2]}
-            camera={{ position: [0, 2, 3], fov: 10 }}
-            className="flex self-center h-auto w-12 overflow-clip place-items-center border-1 border-sand-300 border-t-[10px]"
-          >
-            <ambientLight intensity={0.5} />
-            <spotLight
-              intensity={0.8}
-              angle={0.1}
-              penumbra={1}
-              position={[-10, 15, 4]}
-              castShadow
-            />
-            <Suspense fallback={null}>
-              <Model />
-            </Suspense>
-            <OrbitControls
-              autoRotate
-              enableZoom={false}
-              enablePan={false}
-              target={[-0.23, 0.2, 0.075]}
-            />
-          </Canvas>
+        <div className="flex w-full mb-12 h-[300px] md:h-[500px] hover:cursor-grab active:cursor-grabbing relative">
+          <div className="absolute bottom-0 w-full px-4 py-4 mb-4 ">
+            <div className="flex flex-col justify-center gap-12 md:gap-36 place-items-center">
+              <p className="z-10 px-4 py-4 bg-ink-800/70 border-1 border-pale-100/50">
+                Welcome.
+              </p>
+              <p className="z-10 px-4 py-4 bg-ink-800/70 border-1 border-pale-100/50">
+                A record of the first age.
+              </p>
+            </div>
+          </div>
+          <div className="z-0 w-full">
+            <Canvas
+              shadows
+              dpr={0.4}
+              orthographic
+              camera={{
+                position: [-10, 5, 10],
+                zoom: 125,
+                left: -2,
+                right: 2,
+                top: 2,
+                bottom: 2,
+                near: 1,
+              }}
+              gl={{ antialias: false }}
+              className="flex self-center h-auto w-12 overflow-clip place-items-center border-1 border-sand-300 border-t-[10px]"
+            >
+              <ambientLight intensity={0.5} />
+              <directionalLight
+                color="blue"
+                position={[-10, 10, 10]}
+                intensity={5}
+              />
+              <spotLight
+                intensity={0.8}
+                angle={0.1}
+                penumbra={1}
+                position={[-10, 15, 4]}
+                castShadow
+              />
+              <Suspense fallback={null}>
+                <Model />
+              </Suspense>
+              <OrbitControls
+                autoRotate
+                autoRotateSpeed={-1}
+                enableZoom={false}
+                enablePan={false}
+                target={[-0.23, 0.2, 0.075]}
+              />
+            </Canvas>
+          </div>
         </div>
       </div>
     </div>
