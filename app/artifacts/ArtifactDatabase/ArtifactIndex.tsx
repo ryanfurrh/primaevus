4ed08b8c7cbd9d304dc00ad2c017c1f268d3620a
ryanfurrh <ryanfurrh@gmail.com>
2023-09-28 01:09:07 -0400

    Add Visor and specimen

--- app/artifacts/ArtifactDatabase/ArtifactIndex.tsx
+++ app/artifacts/ArtifactDatabase/ArtifactIndex.tsx
@@ -11,7 +11,7 @@ export const ArtifactIndex: {
   {
     name: "Artifact A",
     file: "models/personal_computer/scene2.gltf",
-    scale: 3.5,
+    scale: 2.5,
     rotation: [0.2, 0.2, 0.5],
     date: "2023/05/15",
     use: "Proximum proficere consentit Mario emergant mares eandem cruciatus Rome clarus. Exoriens dispersa interlunio decidant Scipionibus.  Proximum proficere consentit Mario emergant mares eandem cruciatus Rome clarus. Exoriens dispersa interlunio decidant Scipionibus. Proximum proficere consentit Mario emergant mares eandem cruciatus Rome clarus. Exoriens dispersa interlunio decidant Scipionibus.",
@@ -20,11 +20,20 @@ export const ArtifactIndex: {
   {
     name: "Visor",
     file: "models/visor/visor.gltf",
+    scale: 0.6,
+    position: [0, 0.3, 0],
+    date: "2020/03/27",
+    use: "Aides in harnassing the power of Flux energy.",
+  },
+
+  {
+    name: "Flux Specimen 1",
+    file: "models/flux-specimen-1/flux-specimen-1.gltf",
     scale: 1,
     position: [0, 0.3, 0],
     margin: 1,
     date: "2020/03/27",
-    use: "Aides in harnassing the power of Flux energy.",
+    use: "The raw vital energy or life consciousness that exists in all things.",
   },
 
   {
